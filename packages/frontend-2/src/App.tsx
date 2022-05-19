ebabc3fe6bc8a639fae7c288ed36d343efdce8e6
pepo <pepo.ospina@gmail.com>
2022-05-19 13:27:56 +0200

    add antd and styles

--- packages/frontend-2/src/App.tsx
+++ packages/frontend-2/src/App.tsx
@@ -1,18 +1,25 @@
 import { Provider, createClient } from 'wagmi';
+import { ThemeSwitcherProvider } from 'react-css-theme-switcher';
 
 import './App.css';
 import { LoggedUserContext } from './hooks/useLoggedUser';
 import { MainPage } from './pages/MainPage';
 
+const themes = {
+  dark: './dark-theme.css',
+  light: './light-theme.css',
+};
 
-const client = createClient()
+const client = createClient();
 
 function App() {
   return (
     <div className="App">
       <Provider client={client}>
         <LoggedUserContext>
-          <MainPage></MainPage>
+          <ThemeSwitcherProvider themeMap={themes} defaultTheme={'dark'}>
+            <MainPage></MainPage>
+          </ThemeSwitcherProvider>
         </LoggedUserContext>
       </Provider>
     </div>
