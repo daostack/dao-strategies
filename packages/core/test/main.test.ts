4206f138b945b12df503d1e46e3bc249b3b25ffa
Ido <gershido@gmail.com>
2022-05-24 00:46:46 +0300

    configurable test

--- packages/core/test/main.test.ts
+++ packages/core/test/main.test.ts
@@ -2,39 +2,50 @@ import {
     balancesToObject,
     StrategyComputation,
     WorldConfig,
-} from '@dao-strategies/core';
+    strategies
+} from '../src/dao-strategies-core';
+import type { Strategy_ID, Balances } from '../src/dao-strategies-core';
 import { BigNumber } from 'ethers';
 import 'dotenv/config';
+import { performance } from 'perf_hooks';
 
-let token: string | undefined;
-let config: WorldConfig;
-let strategyComp: StrategyComputation;
 
-test('init', () => {
-    token = process.env.GITHUB_TOKEN;
-    if (token == undefined) {
-        throw 'Github token not defined';
-    }
-    config = { GITHUB_TOKEN: token as string };
-    strategyComp = new StrategyComputation(config);
-});
+const strategyName = (process.argv.find((arg) => arg.includes('--strategy=')) || '--strategy=GH_PRS_REACTIONS_WEIGHED').split('--strategy=').pop();
+if (strategyName == undefined) {
+    throw new Error("Error: strategy name is undefined");
+}
 
-test('basic test', async () => {
-    const result = await strategyComp.runStrategy('GH_PRS_REACTIONS_WEIGHED', {
-        repositories: [{ owner: 'gershido', repo: 'test-github-api' }],
-        timeRange: { start: 1640998800, end: 1652437383 },
+describe(`\nTest strategy "${strategyName}"`, () => {
+
+    let token: string | undefined;
+    let config: WorldConfig;
+    let strategyComp: StrategyComputation;
+    let shares: Balances;
+
+    test('init', () => {
+        token = process.env.GITHUB_TOKEN;
+        if (token == undefined) {
+            throw 'Github token not defined';
+        }
+        config = { GITHUB_TOKEN: token as string };
+        strategyComp = new StrategyComputation(config);
     });
 
-    console.log(JSON.stringify(balancesToObject(result)));
-    expect(balancesToObject(result)).toEqual({ "gershido": "1000000000000000000" });
-});
+    test('run strategy', async () => {
+        shares = await strategyComp.runStrategy(
+            strategyName as Strategy_ID,
+            strategies[strategyName as Strategy_ID].strategyInfo.exapmle_Params
+        );
 
-test('geth', async () => {
-    const result = await strategyComp.runStrategy('GH_PRS_REACTIONS_WEIGHED', {
-        repositories: [{ owner: 'ethereum', repo: 'go-ethereum' }],
-        timeRange: { start: 1651400578, end: 1653215165 },
+        console.log(JSON.stringify(balancesToObject(shares)));
+    }, 360e3);
+
+    test('check sum of shares equals 1e18', () => {
+        let sum: BigNumber = BigNumber.from(0);
+        for (let amount of shares.values()) {
+            sum = sum.add(amount);
+        }
+
+        expect(sum.eq(BigNumber.from("1000000000000000000"))).toEqual(true);
     });
-
-    console.log(JSON.stringify(balancesToObject(result)));
-    expect(1).toBe(1);
-}, 300e3);
+});
