238a3ab72e7f88a35003c2dd96c703cfc2dc6f99
Ido <gershido@gmail.com>
2022-05-29 10:24:19 +0300

    optimize pull request pagination

--- packages/core/src/strategies/github/utils.ts
+++ packages/core/src/strategies/github/utils.ts
@@ -33,30 +33,66 @@ export async function getPrsInRepo(
   mergedFrom?: number,
   mergedTo?: number
 ): Promise<PullRequestListData> {
-  const allPulls: PullRequestListData = [];
-  const iterator = world.github.paginate.iterator(
-    world.github.rest.pulls.list,
-    {
-      ...repo,
-      state: 'all',
-      per_page: 100,
-    }
-  );
+  let allPulls: PullRequestListData = [];
 
-  // iterate through each response
-  for await (const { data: pulls } of iterator) {
-    for (const pull of pulls) {
+  let firstReq = await world.github.rest.pulls.list({
+    ...repo,
+    state: 'all',
+    per_page: 100,
+  });
+
+  if (firstReq.headers.link == undefined) { // no pagination needed
+    firstReq.data.forEach(pull => {
       if (mergedFrom != undefined && mergedTo != undefined && mergedFrom < mergedTo) { // if filter by merge time
         if (
           pull.merged_at != null &&
           mergedFrom <= toTimeStamp(pull.merged_at) &&
           mergedTo >= toTimeStamp(pull.merged_at)) {
-          allPulls.push(pull);;
+          allPulls.push(pull);
         }
       }
       else {
         allPulls.push(pull);
       }
+    });
+  }
+  else {
+    let numPagesReg = firstReq.headers.link?.match(/<.*page=(\d+).*>; rel="last"/);
+    if (numPagesReg != undefined && numPagesReg != null) {
+      let numPages: number = Number(numPagesReg[1]);
+      console.log("Num Pages:", numPages);
+      let reqs = [];
+      reqs.push(firstReq);
+      for (let i = 2; i < numPages; i++) {
+        reqs.push(world.github.rest.pulls.list({
+          ...repo,
+          state: 'all',
+          per_page: 100,
+          page: i
+        }));
+      }
+
+      await Promise.all(reqs)
+        .then(responses => {
+          for (let response of responses) {
+            for (let pull of response.data) {
+              if (mergedFrom != undefined && mergedTo != undefined && mergedFrom < mergedTo) { // if filter by merge time
+                if (
+                  pull.merged_at != null &&
+                  mergedFrom <= toTimeStamp(pull.merged_at) &&
+                  mergedTo >= toTimeStamp(pull.merged_at)) {
+                  allPulls.push(pull);;
+                }
+              }
+              else {
+                allPulls.push(pull);
+              }
+            }
+          }
+        })
+    }
+    else {
+      throw new Error("Error: cannot paginate all pull requests");
     }
   }
 
