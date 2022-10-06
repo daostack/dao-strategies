41b800a2a69f227956afdc802030694334353803
Johannes Mueller <muellerjohannes93@gmail.com>
2022-10-06 13:28:38 +0200

    added helpicon position to field label added past campaign time moved duration time logic  to date manager

--- packages/frontend/src/components/CampaignRewardsTime.tsx
+++ packages/frontend/src/components/CampaignRewardsTime.tsx
@@ -1,9 +1,11 @@
 import { CampaignFundersRead, campaignInstance, Page } from '@dao-strategies/core';
 import { parseEther } from 'ethers/lib/utils';
-import { Box, Spinner } from 'grommet';
+import { Box, Spinner, Text } from 'grommet';
 import { StatusGood } from 'grommet-icons';
 import { FC } from 'react';
 import { useCampaignContext } from '../hooks/useCampaign';
+import { useNow } from '../hooks/useNow';
+import { FieldLabel } from '../pages/create/field.label';
 import { DateManager } from '../utils/date.manager';
 
 import { Countdown } from './Countdown';
@@ -15,14 +17,17 @@ export interface CampaignRewardsTimeI {
 }
 
 export const CampaignRewardsTime: FC<CampaignRewardsTimeI> = (props: CampaignRewardsTimeI) => {
-  const { campaign } = useCampaignContext();
   const { execDate, alreadyExecuted } = props;
+  const { now } = useNow();
 
   return (
-
-    <Box>
-      <Countdown to-date={execDate}> Shares distributed in: </Countdown>
+    <Box style={{ color: '#878787' }}>
+      {alreadyExecuted
+        ? (<FieldLabel helpIconPosition='left' help="true" label={`Campaign shares successfully distributed ${now?.intervalDuration(new Date(), execDate).days} days ago`}></FieldLabel>)
+        : (<Countdown to-date={execDate} text='Shares distributed in:'>  </Countdown>)}
     </Box>
+
+
   )
 };
 
--- packages/frontend/src/components/Countdown.tsx
+++ packages/frontend/src/components/Countdown.tsx
@@ -1,20 +1,19 @@
 import { Box, Spinner } from 'grommet';
 import { FC, useEffect, useState } from 'react';
 import { useNow } from '../hooks/useNow';
-import { IElement } from './styles/BasicElements';
-import { formatDuration, intervalToDuration } from 'date-fns'
-import { DateManager } from '../utils/date.manager';
-import { toDate } from 'date-fns/esm';
+import { FieldLabel } from '../pages/create/field.label';
+import { AppRemainingTime, IElement } from './styles/BasicElements';
+
 
 const DEBUG = true;
 
 export interface CountdownI extends IElement {
   'to-date': number;
-  children?: React.ReactNode;
+  text?: string;
 }
 
 export const Countdown: FC<CountdownI> = (props: CountdownI) => {
-  const [remaining, setRemaining] = useState<Duration>();
+  const [remaining, setRemaining] = useState<Duration | undefined>(undefined);
   const { now } = useNow();
   const execDate = new Date(props['to-date'] * 1000);
 
@@ -24,11 +23,9 @@ export const Countdown: FC<CountdownI> = (props: CountdownI) => {
   useEffect(() => {
     const getRem = () => {
       if (DEBUG) console.log('Countdown - Interval', { now });
-      let duration = intervalToDuration({
-        start: execDate,
-        end: new Date(),
-      })
-      setRemaining(duration)
+      if (!now) setRemaining(undefined);
+
+      setRemaining(now?.intervalDuration(new Date(), execDate))
     };
     getRem();
     const interval = setInterval(getRem, 1000);
@@ -39,25 +36,14 @@ export const Countdown: FC<CountdownI> = (props: CountdownI) => {
 
   return (
     <>
-      <Box style={{ width: '100%', textAlign: 'start' }} justify="start" align="start" direction="column">
+      <Box style={{ width: '100%', textAlign: 'start' }} justify="start" align="start" >
         {loading ? <Spinner></Spinner> : (<Box>
 
           {remaining !== undefined ? (
-            <div style={{ flexDirection: 'column', gap: 'small' }}>
-              {props.children}
-              <div style={{ marginLeft: '8px', marginRight: '8px', display: 'inline-block' }}>
-                <strong>{remaining.days}</strong> <span> days</span>
-              </div>
-              <div style={{ marginLeft: '8px', marginRight: '8px', display: 'inline-block' }}>
-                <strong>{remaining.hours}</strong> <span> hours</span>
-              </div>
-              <div style={{ marginLeft: '8px', marginRight: '8px', display: 'inline-block' }}>
-                <strong>{remaining.minutes}</strong> <span> minutes</span>
-              </div>
-              <div style={{ marginLeft: '8px', marginRight: '8px', display: 'inline-block' }}>
-                <strong>{remaining.seconds}</strong> <span> seconds</span>
-              </div>
-            </div>
+            <Box gap='10px' direction='row'>
+              <FieldLabel label={props.text ?? ''} helpIconPosition={'left'} />
+              <AppRemainingTime compactFormat={false} remainingTime={remaining} />
+            </Box>
           ) : ''}
         </Box>)
         }
