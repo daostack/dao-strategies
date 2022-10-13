ef1a1f45b5f68dc56f24dfb880fa25d244ca13ad
pepo <pepo.ospina@gmail.com>
2022-10-13 12:54:14 +0200

    clean-up use scroll for all windows

--- packages/frontend/src/components/landing/EmbeddedLanding.tsx
+++ packages/frontend/src/components/landing/EmbeddedLanding.tsx
@@ -17,9 +17,9 @@ export const MAX_WIDTH_LANDING = 1600;
 
 export const EmbeddedLanding: FC<IEmbeddedLanding> = (props: IEmbeddedLanding) => {
   return (
-    <Box style={{}}>
-      <Box style={{ maxWidth: `${MAX_WIDTH_LANDING}px`, margin: `${HEADER_HEIGHT}px auto 0 auto`, padding: '0px 5vw' }}>
-        <Banner bannerLink="https://t.me/+a0mIY6gHOG00OGU0"></Banner>
+    <Box style={{}} align="center">
+      <Box style={{ maxWidth: `${MAX_WIDTH_LANDING}px`, margin: `${HEADER_HEIGHT}px 0 0 `, padding: '0px 5vw' }}>
+        <Banner bannerLink="https://t.me/+a0mIY6gHOG00OGU0" style={{ marginTop: '32px' }}></Banner>
         <Box style={{ height: '10vw', maxHeight: '150px' }}></Box>
         <Hero style={{}}></Hero>
         <Box style={{ height: '15vw', maxHeight: '200px' }}></Box>
@@ -35,7 +35,7 @@ export const EmbeddedLanding: FC<IEmbeddedLanding> = (props: IEmbeddedLanding) =
       <Box style={{ backgroundColor: constants.lightBackground }} align="center">
         <HowItWorks style={{}}></HowItWorks>
         <Box style={{ height: '10vw', maxHeight: '300px' }}></Box>
-        <ComingNext style={{ maxWidth: `${MAX_WIDTH_LANDING}px` }}></ComingNext>
+        <ComingNext style={{ maxWidth: `${MAX_WIDTH_LANDING}px`, padding: '0 5vw' }}></ComingNext>
         <Box style={{ height: '10vw', maxHeight: '300px' }}></Box>
       </Box>
       <Footer style={{}}></Footer>
